2221fb7083e81592429b9d76edbf011f63f34487
omnia <Omniaabdelnasser067@gmail.com>
2024-12-02 16:55:30 +0200

    omnia

--- my-app/src/componants/nav.js
+++ my-app/src/componants/nav.js
@@ -6,14 +6,14 @@ return(
     <h2>NEW<span>IKEA</span></h2>
   </div>
   <ul className="nav-links" id="nav-links">
-    <li><a style={{color: '#e2bb9c'}} href="/"><b>Home</b></a></li>
+    <li><a style={{color: '#fe8a06'}} href="/"><b>Home</b></a></li>
     <li><a href="/">products</a></li>
     <li><a href="/componants/contact">Contact Us</a></li>
     <li><a href="/componants/about">About </a></li>
   </ul>
   <div className="actions">
     <input type="text" id="search" placeholder="Search..." />
-    <button id="theme-toggle3"><i className="fa-solid fa-circle-half-stroke" /></button>
+    {/* <button id="theme-toggle3"><i className="fa-solid fa-circle-half-stroke" /></button> */}
   </div>
   <div className="menu-icon" id="menu-icon">☰</div>
 </nav>
